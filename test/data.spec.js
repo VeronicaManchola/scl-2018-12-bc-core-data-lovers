c8cfce810a0bc4cafed8090428ac4ccf7f28cd2f
mirandanatalia <lya_latina@hotmail.com>
2019-01-14 13:01:52 -0300

    toma data desde pokemon.json

--- test/data.spec.js
+++ test/data.spec.js
@@ -1,10 +1,9 @@
 global.window = global;
 global.assert = require("chai").assert;
+global.POKEMON = require("../src/data/pokemon/pokemon.json");
 require('../src/data.js');
-require("../src/data/pokemon/pokemon.js");
 
-const pokemones = window.POKEMON.pokemon;
-const data = window.pokemonData;
+const pokemones = global.POKEMON.pokemon;
 
 describe("data", () => {
   it("deberia ser un objeto", () => {
@@ -18,11 +17,11 @@ describe("filterData", () => {
   });
 
   it(`deberia retornar como primer pokemon de clase Planta a Bulbasaur`, () => {
-    global.assert.equal(window.pokemonData.filterData(pokemones, "Grass").length, "14");
+    global.assert.lengthOf(window.pokemonData.filterData(pokemones, "Grass"), 14);
   });
 
   it(`deberia retornar todos los pokemones sin importar el orden previamente seleccionado`, () => {
-    global.assert.equal(window.pokemonData.filterData(pokemones, "0"), pokemones);
+    global.assert.sameDeepOrderedMembers(window.pokemonData.filterData(pokemones, "0"), pokemones);
   });
 });
 
@@ -32,45 +31,45 @@ describe("sortData", () => {
   });
 
   it("deberia retornar el numero 001 como el primer numero ordenado de 1-151", () => {
-    global.assert.equal(window.pokemonData.sortData(pokemones, "num"), "1-151"[0].num, "001");
+    global.assert.deepStrictEqual(window.pokemonData.sortData(pokemones, "lower")[0].num, "001");
   });
 
   it("debería retornar el numero 001 antes de 002 con un orden lower", () => {
-    global.assert.equal(window.pokemonData.sortData(pokemones, "lower")[0].num < pokemones[1].num, "001" < "002");
+    global.assert.operator(window.pokemonData.sortData(pokemones, "lower")[0].num, "<" , pokemones[1].num);
   });
 
   it("deberia retornar el numero 151 como el primer numero ordenado de 151-1", () => {
-    global.assert.equal(window.pokemonData.sortData(pokemones, "num"), "151-1"[0].num, "151");
+    global.assert.deepStrictEqual(window.pokemonData.sortData(pokemones, "higher")[0].num, "151");
   });
 
   it("debería retornar el numero 151 antes de 150 con un orden higher", () => {
-    global.assert.equal(window.pokemonData.sortData(pokemones, "higher")[0].num > pokemones[1].num, "151" > "150");
+    global.assert.operator(window.pokemonData.sortData(pokemones, "higher")[0].num, ">", pokemones[1].num);
   });
 
   it("deberia retornar el nombre Abra como el primer nombre ordenado de a-z", () => {
-    global.assert.equal(window.pokemonData.sortData(pokemones, "name"), "A-Z"[0].name, "Abra");
+    global.assert.deepStrictEqual(window.pokemonData.sortData(pokemones, "az")[0].name, "Abra");
   });
 
   it("debería retornar el nombre Abra antes de Aerodactil con un orden a-z", () => {
-    global.assert.equal(window.pokemonData.sortData(pokemones, "az")[0].name < pokemones[1].name, "Abra" < "Aerodactil");
+    global.assert.operator(window.pokemonData.sortData(pokemones, "az")[0].name, "<", pokemones[1].name);
   });
 
   it("deberia retornar el nombre Zubat como el primer nombre ordenado de z-a", () => {
-    global.assert.equal(window.pokemonData.sortData(pokemones, "name"), "Z-A"[0].name, "Zubat");
+    global.assert.deepStrictEqual(window.pokemonData.sortData(pokemones, "za")[0].name, "Zubat");
   })
 
   it("debería retornar el nombre Zubat antes de Zapdos con un orden z-a", () => {
-    global.assert.equal(window.pokemonData.sortData(pokemones, "za")[0].name > pokemones[1].name, "Zubat" > "Zapdos");
+    global.assert.operator(window.pokemonData.sortData(pokemones, "za")[0].name, ">", pokemones[1].name);
+  });
+});
+
+describe("computeStats", () => {
+  it("deberia ser una funcion", () => {
+    global.assert.equal(typeof window.pokemonData.computeStats, "function");
   });
 
-  describe("computeStats", () => {
-    it("deberia ser una funcion", () => {
-      global.assert.equal(typeof window.pokemonData.computeStats, "function");
-    });
-
-    it("debiera retornar el numero 14 para la suma de los pokemones del tipo Planta", () => {
-      global.assert(window.pokemonData.computeStats(pokemones, "Grass"), "14");
-    });
+  it("debiera retornar el numero de los pokemones en la data", () => {
+    global.assert.deepStrictEqual(window.pokemonData.computeStats(pokemones), 151);
   });
 });
 
